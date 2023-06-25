d0d2ac55d5683b817e71823130c32e29d165af39
Șulic Octavian <octavius1992@gmail.com>
2023-06-25 12:45:45 +0300

    Add project files.

---
+++ BettingApp/Guy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerClasses
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+
+        public int Cash { get; set; }
+
+        public Player(string name, int cash) { Name = name; Cash = cash; }
+
+        public void WriteMyInfo()
+        {
+            Console.WriteLine($"My name is {Name} and I have {Cash} dollars.");
+        }
+
+        public int GiveCash(int amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine(Name + " says: " + amount + " isn't a valid amount");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine(Name + " says: " + amount + " is too much, I don't have that much money!");
+                return 0;
+            }
+
+            Cash -= amount;
+            return amount;
+        }
+
+        public void ReceiveCash(int amount)
+        {
+            Cash += amount;
+            Console.WriteLine($"{Name}: I got {amount} dollars! Awesome!");
+        }
+    }
+}
---
+++ BettingApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using PlayerClasses;
+
+Random random = new Random();
+
+double odds = 0.75;
+
+Player player = new Player("Tony", 100);
+
+    Console.WriteLine("Welcome to NeverWin Casino, where the odds are stacked against you!");
+    Console.WriteLine();
+
+
+while (player.Cash > 0)
+{
+
+    player.WriteMyInfo();
+    Console.Write($"How much money would you like to bet {player.Name}? ");
+    string howMuch = Console.ReadLine();
+    Console.WriteLine();
+
+    if (int.TryParse(howMuch, out int amount))
+    {
+        int pot = player.GiveCash(amount) * 2;
+
+        double number = random.NextDouble();
+
+        if(number > odds)
+        {
+            player.ReceiveCash(pot);
+            Console.WriteLine("Hey, you won: " + pot + " dollars! Congratulations!");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("You lost this round, better luck next time!");
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.WriteLine("Ohhh, I'm sorry, but you ran out of money.\n\nWe also have a pawn shop if that might help you!");
